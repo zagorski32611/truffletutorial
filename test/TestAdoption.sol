80ac3f0a885faee3f249490f7e4a857198cb55ea
zagorski32611 <j.j.zagorski@gmail.com>
2017-11-26 08:42:32 -0500

    added 3 tests, all passing

--- test/TestAdoption.sol
+++ test/TestAdoption.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.4.11;
 
-import "truffle/Assert.sol"
+import "truffle/Assert.sol";
 
-import "truffle/DeployedAddresses.sol"
+import "truffle/DeployedAddresses.sol";
 
-import "../contracts/Adoption.sol"
+import "../contracts/Adoption.sol";
 
 contract TestAdoption {
   Adoption adoption = Adoption(DeployedAddresses.Adoption());
@@ -16,7 +16,28 @@ contract TestAdoption {
     uint expected = 8;
 
     Assert.equal(returnedId, expected, "Adoption of pet ID 8 should be recorded.");
-    
+
+  }
+
+  // testing retrieval of a single pet's owner
+  function testGetAdopterAddressById() {
+    // Expected owner is this contract
+    address expected = this;
+
+    address adopter = adoption.adopters(8);
+
+    Assert.equal(adopter, expected, "Owner of pet ID 8 should be recoreded");
+  }
+
+  // testing retrieval of all pet owners
+  function testGetAdopterAddressByIdInArray() {
+    // Expected owner in this contract
+    address expected = this;
+
+    // Store Adopters in memory rather than contract's storage
+    address[16] memory adopters = adoption.getAdopters();
+
+    Assert.equal(adopters[8], expected, "Owner of pet ID 8 should be recoreded");
   }
 
 }
